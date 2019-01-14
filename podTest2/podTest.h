ad68fea8c694d11aeb2800871c662dce9e2510dc
heqingzhao <heqza@litsoft.com.cn>
2019-01-14 11:47:13 +0800

    创建pod文件

---
+++ podTest2/podTest.h
@@ -0,0 +1,17 @@
+//
+//  podTest.h
+//  podTest2
+//
+//  Created by qingzhao on 2019/1/14.
+//  Copyright © 2019年 qingzhao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface podTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ podTest2/podTest.m
@@ -0,0 +1,13 @@
+//
+//  podTest.m
+//  podTest2
+//
+//  Created by qingzhao on 2019/1/14.
+//  Copyright © 2019年 qingzhao. All rights reserved.
+//
+
+#import "podTest.h"
+
+@implementation podTest
+
+@end
